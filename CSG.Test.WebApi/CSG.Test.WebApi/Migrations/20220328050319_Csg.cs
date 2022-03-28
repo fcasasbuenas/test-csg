9cdd1768c1acd6f1deb19c7e72e253b02204dcb2
fcasasbuenas <fcasasbuenas@users.noreply.github.com>
2022-03-28 00:04:58 -0500

    - Added first DB migration

---
+++ CSG.Test.WebApi/CSG.Test.WebApi/Migrations/20220328050319_Csg.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CSG.Test.WebApi.Migrations
+{
+    public partial class Csg : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Departments",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Location = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Departments", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Salary = table.Column<long>(nullable: false),
+                    DepartmentId = table.Column<long>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Employees_Departments_DepartmentId",
+                        column: x => x.DepartmentId,
+                        principalTable: "Departments",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Departments",
+                columns: new[] { "Id", "Location", "Name" },
+                values: new object[,]
+                {
+                    { 1L, "Sydney", "Executive" },
+                    { 2L, "Sydney", "Production" },
+                    { 3L, "Cape Town", "Resources" },
+                    { 4L, "Texas", "Technical" },
+                    { 5L, "Paris", "Management" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "Id", "DepartmentId", "Name", "Salary" },
+                values: new object[,]
+                {
+                    { 1L, 1L, "Candice", 4685L },
+                    { 4L, 1L, "Scarlet", 2350L },
+                    { 2L, 2L, "Julia", 2559L },
+                    { 3L, 4L, "Bob", 4405L },
+                    { 5L, 4L, "Ilena", 1151L }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employees_DepartmentId",
+                table: "Employees",
+                column: "DepartmentId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+
+            migrationBuilder.DropTable(
+                name: "Departments");
+        }
+    }
+}
